2a70637472e44c8f7a97c4abcd3adc9389a6edc4
smaldonado411 <smaldonado@distelsa.com.gt>
2025-10-30 17:12:13 -0600

    Add bencode decoding for dictionaries

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,21 @@
-use serde_json::{self, Number};
+use serde_json::{self, Map, Number, Value};
 use std::env;
 
-// Available if you need it!
-// use serde_bencode
-
-fn decode_bencoded_value(encoded_value: &str) -> (serde_json::Value, String) {
-    // If encoded_value starts with a digit, it's a number
+fn decode_bencoded_value(encoded_value: &str) -> (Value, String) {
     if encoded_value.chars().next().unwrap().is_digit(10) {
         // Example: "5:hello" -> "hello"
         let colon_index = encoded_value.find(':').unwrap();
         let number_string = &encoded_value[..colon_index];
         let number = number_string.parse::<usize>().unwrap();
         let string = &encoded_value[colon_index + 1..colon_index + 1 + number];
-        return (serde_json::Value::String(string.to_string()), encoded_value[colon_index + 1 + number..].into());
+        return (Value::String(string.to_string()), encoded_value[colon_index + 1 + number..].into());
     } else if encoded_value.chars().next().unwrap() == 'i' {
         // Example: "i-52e" -> -52
         let i_index = encoded_value.find('i').unwrap();
         let e_index = encoded_value.find('e').unwrap();
         let number_string = &encoded_value[i_index + 1..e_index];
         let number = number_string.parse::<i64>().unwrap();
-        return (serde_json::Value::Number(Number::from(number)), encoded_value[e_index+1..].into());
+        return (Value::Number(Number::from(number)), encoded_value[e_index+1..].into());
     } else if encoded_value.chars().next().unwrap() == 'l' {
         // Example: "l5:helloi52ee" -> ["hello",52]
         let mut current_string = String::from(&encoded_value[1..]);
@@ -29,8 +25,23 @@ fn decode_bencoded_value(encoded_value: &str) -> (serde_json::Value, String) {
             value_list.push(value);
             current_string = rest;
         }
-        current_string = current_string[1..].into();
-        return (serde_json::Value::Array(value_list), current_string)
+        return (Value::Array(value_list), current_string[1..].into())
+    } else if encoded_value.chars().next().unwrap() == 'd' {
+        // Example: d3:foo3:bar5:helloi52ee
+        let mut map = Map::new();
+        let mut current_string = String::from(&encoded_value[1..]);
+        while current_string.chars().next().unwrap() != 'e' {
+            let (key, rest) = decode_bencoded_value(&current_string);
+            current_string = rest;
+            if let Value::String(key) = key {
+                let (val, rest) = decode_bencoded_value(&current_string);
+                map.insert(key, val);
+                current_string = rest;
+            } else {
+                panic!("Dictionary keys should be Strings.");
+            }
+        }
+        return (Value::Object(map), current_string[1..].into());
     } else {
         panic!("Unhandled encoded value: {}", encoded_value)
     }
