b9212442290989a0bce5536a5166ff2a2ca0463d
sebas411 <sebasma411@gmail.com>
2025-10-31 22:12:06 -0600

    Refactor code to use self managed Values for futureproofing

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,45 @@
-use serde_json::{self, Map, Number, Value};
+mod modules;
 use std::env;
+use crate::modules::value::{Map, Value};
 
-fn decode_bencoded_value(encoded_value: &str) -> (Value, String) {
-    if encoded_value.chars().next().unwrap().is_digit(10) {
+fn is_digit(c: char) -> bool {
+    c >= '0' && c <= '9'
+}
+
+fn find_index(vector: &[u8], c: char) -> usize {
+    vector.iter().position(|u| *u == c as u8).unwrap()
+}
+
+fn decode_bencoded_value(encoded_value: &[u8]) -> (Value, Vec<u8>) {
+    if is_digit(encoded_value[0] as char) {
         // Example: "5:hello" -> "hello"
-        let colon_index = encoded_value.find(':').unwrap();
-        let number_string = &encoded_value[..colon_index];
+        let colon_index = find_index(encoded_value, ':');
+        let number_string = String::from_utf8(encoded_value[..colon_index].to_vec()).unwrap();
         let number = number_string.parse::<usize>().unwrap();
         let string = &encoded_value[colon_index + 1..colon_index + 1 + number];
-        return (Value::String(string.to_string()), encoded_value[colon_index + 1 + number..].into());
-    } else if encoded_value.chars().next().unwrap() == 'i' {
+        return (Value::String(string.to_vec()), encoded_value[colon_index + 1 + number..].to_vec());
+    } else if encoded_value[0] as char == 'i' {
         // Example: "i-52e" -> -52
-        let i_index = encoded_value.find('i').unwrap();
-        let e_index = encoded_value.find('e').unwrap();
-        let number_string = &encoded_value[i_index + 1..e_index];
+        let i_index = find_index(encoded_value, 'i');
+        let e_index = find_index(encoded_value, 'e');
+        let number_string = String::from_utf8(encoded_value[i_index + 1..e_index].to_vec()).unwrap();
         let number = number_string.parse::<i64>().unwrap();
-        return (Value::Number(Number::from(number)), encoded_value[e_index+1..].into());
-    } else if encoded_value.chars().next().unwrap() == 'l' {
+        return (Value::Int(number), encoded_value[e_index+1..].into());
+    } else if encoded_value[0] as char == 'l' {
         // Example: "l5:helloi52ee" -> ["hello",52]
-        let mut current_string = String::from(&encoded_value[1..]);
+        let mut current_string = encoded_value[1..].to_vec();
         let mut value_list = vec![];
-        while current_string.chars().next().unwrap() != 'e' {
+        while current_string[0] as char != 'e' {
             let (value, rest) = decode_bencoded_value(&current_string);
             value_list.push(value);
             current_string = rest;
         }
-        return (Value::Array(value_list), current_string[1..].into())
-    } else if encoded_value.chars().next().unwrap() == 'd' {
+        return (Value::List(value_list), current_string[1..].into())
+    } else if encoded_value[0] as char == 'd' {
         // Example: d3:foo3:bar5:helloi52ee
         let mut map = Map::new();
-        let mut current_string = String::from(&encoded_value[1..]);
-        while current_string.chars().next().unwrap() != 'e' {
+        let mut current_string = encoded_value[1..].to_vec();
+        while current_string[0] as char != 'e' {
             let (key, rest) = decode_bencoded_value(&current_string);
             current_string = rest;
             if let Value::String(key) = key {
@@ -41,9 +50,9 @@ fn decode_bencoded_value(encoded_value: &str) -> (Value, String) {
                 panic!("Dictionary keys should be Strings.");
             }
         }
-        return (Value::Object(map), current_string[1..].into());
+        return (Value::Map(map), current_string[1..].into());
     } else {
-        panic!("Unhandled encoded value: {}", encoded_value)
+        panic!("Unhandled encoded value: {}", String::from_utf8(encoded_value.to_vec()).unwrap())
     }
 }
 
@@ -51,11 +60,14 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let command = &args[1];
 
-    if command == "decode" {
-        let encoded_value = &args[2];
-        let (decoded_value, _) = decode_bencoded_value(encoded_value);
-        println!("{}", decoded_value.to_string());
-    } else {
-        println!("unknown command: {}", args[1])
+    match command.as_str() {
+        "decode" => {
+            let encoded_value = &args[2];
+            let (decoded_value, _) = decode_bencoded_value(encoded_value.as_bytes());
+            println!("{}", decoded_value.to_string());
+        },
+        _ => {
+            println!("unknown command: {}", args[1])
+        },
     }
 }
---
+++ src/modules/value.rs
@@ -0,0 +1,60 @@
+use std::collections::BTreeMap;
+
+pub struct Map(BTreeMap<Vec<u8>, Value>);
+
+impl Map {
+    pub fn new() -> Self {
+        Self(BTreeMap::new())
+    }
+    pub fn insert(&mut self, k: Vec<u8>, v: Value) {
+        self.0.insert(k, v);
+    }
+    pub fn to_string(&self) -> String {
+        let mut map_string = String::from('{');
+        for (i, (k, v)) in self.0.iter().enumerate() {
+            let key_string = String::from_utf8(k.clone()).unwrap_or("String not in utf8".into());
+            map_string.push_str(&format!("\"{}\":{}", key_string, v.to_string()));
+            if i != self.0.len() - 1 {
+                map_string.push(',');
+            }
+        }
+        map_string.push('}');
+        map_string
+    }
+}
+
+pub enum Value {
+    String(Vec<u8>),
+    Int(i64),
+    List(Vec<Value>),
+    Map(Map),
+}
+
+impl Value {
+    pub fn to_string(&self) -> String {
+        match self {
+            Self::String(value) => {
+                if let Ok(string_value) = String::from_utf8(value.clone()) {
+                    format!("\"{}\"", string_value)
+                } else {
+                    "String not in utf8".into()
+                }
+            },
+            Self::Int(value) => value.to_string(),
+            Self::List(list) => {
+                let mut output = String::from("[");
+                for (i, val) in list.iter().enumerate() {
+                    output.push_str(&val.to_string());
+                    if i != list.len() - 1 {
+                        output.push(',');
+                    }
+                }
+                output.push(']');
+                output
+            },
+            Self::Map(map) => {
+                map.to_string()
+            },
+        }
+    }
+}
